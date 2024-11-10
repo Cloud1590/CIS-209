702cf102d818f2e48b8a6bffbd59aedb2b05fccc
Cloud1590 <daniel4vance@gmail.com>
2024-11-09 22:38:07 -0600

    Add Week 10 and Week 5 files; implement Week 7 HTML for SEO discussion and update Week 8 DB.py with networking concepts

---
+++ Week 5/DB.py
@@ -0,0 +1,6 @@
+'''Answer the following question:
+How would you explain to users the types of attacks that they are susceptible to at home?
+Your response should be no less than two paragraphs in length.'''
+
+# Answer:
+

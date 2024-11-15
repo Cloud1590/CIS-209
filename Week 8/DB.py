9cf964f501ec3cb1422c2682568aca9e6975adeb
Cloud1590 <daniel4vance@gmail.com>
2024-11-15 15:26:23 -0600

    Update Week 8 DB.py with SDLC overview; modify Week 7 DB.html for SEO content and links; remove Week 7 DB2.html

---
+++ Week 8/DB.py
@@ -0,0 +1,15 @@
+'''
+
+
+The Software Development Life Cycle (SDLC) contains five key phases: Requirement Analysis, Design, Implementation, Testing, and Maintenance. In the Requirement Analysis phase, software needs are gathered and analyzed to define detailed software requirements. During the Design phase, the software's architecture and system designs are created. Unified Modeling Language (UML) diagrams are often utilized to create visual representations of the system's structure and components. 
+
+In the Implementation phase, developers translate design documents into actual code. Integrated Development Environments (IDEs) like Visual Studio Code or PyCharm are commonly used to code and debug. The Testing phase involves verifying that the software meets all specified requirements and identifying any defects or bugs still present. If any additional bugs are found, it usually goes back to the development team. Finally, in the Maintenance phase, the software is monitored and updated to fix issues or adapt to new requirements. Version control systems like Git, along with issue tracking tools are essential for managing ongoing maintenance tasks.
+
+
+Sources: 
+•	Chapter 10 in the textbook.
+•	Module 10 PowerPoint slides
+
+
+
+'''
